4bedc02c37cd741d093d6f59110c225bb888c837
Tóth András <tandris@Toths-iMac-PC.local>
2016-09-02 17:20:45 +0200

    configure selenium

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+set -e
+
+gitlab-runner register -r $GITLAB_CI_TOKEN -u $GITLAB_CI_URL --executor $GITLAB_CI_EXECUTOR --name $GITLAB_CI_NAME -n
+
+gitlab-runner run
+
+
 export DISPLAY=:99
 Xvfb :99 -shmem -screen 0 1366x768x16 &
 x11vnc -passwd secret -display :99 -N -forever &
