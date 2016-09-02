553b4cfe425ab023ff6834e1de6ca2a2dbf420e4
Tóth András <tandris@Toths-iMac-PC.local>
2016-09-02 22:43:44 +0200

    configure selenium

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 export DISPLAY=:99
-xvfb-run --server-args="-screen 0, 1366x768x24" selenium-standalone start
+#xvfb-run --server-args="-screen 0, 1366x768x24" selenium-standalone start
 
 gitlab-runner register -r $GITLAB_CI_TOKEN -u $GITLAB_CI_URL --executor $GITLAB_CI_EXECUTOR --name $GITLAB_CI_NAME -n
 
