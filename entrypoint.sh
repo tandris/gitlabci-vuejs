6d0db8ce2923c4ea47b1ed68f314ebc409b57144
Tóth András <tandris@Toths-iMac-PC.local>
2016-09-02 19:14:52 +0200

    configure selenium

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,8 @@
 set -e
 
 export DISPLAY=:99
-Xvfb :99 -shmem -screen 0 1366x768x16 &
-x11vnc -passwd secret -display :99 -N -forever &
+xvfb-run --server-args="-screen 0, 1366x768x24" selenium-standalone start
 
 gitlab-runner register -r $GITLAB_CI_TOKEN -u $GITLAB_CI_URL --executor $GITLAB_CI_EXECUTOR --name $GITLAB_CI_NAME -n
 
 gitlab-runner run
-
-
-
-# selenium must be started by a non-root user otherwise chrome can't start
-su - seleuser -c "xvfb-run --server-args=\"-screen 0, 1366x768x24\" selenium-standalone start"
