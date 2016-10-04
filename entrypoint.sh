4fa949fb180fddcc433701d88184006c655bc121
András Tóth <tandris@users.noreply.github.com>
2016-10-04 12:13:05 +0200

    Add maven and sonar config

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+#Maven Sonar config 
+cd /home/gitlab-runner/.m2/
+sed -i 's/SONAR_HOST_URL/'$SONAR_HOST_URL'/' settings.xml
+sed -i 's/SONAR_JDBC_URL/'$SONAR_JDBC_URL'/' settings.xml
+sed -i 's/SONAR_JDBC_USER/'$SONAR_JDBC_USER'/' settings.xml
+sed -i 's/SONAR_JDBC_PWD/'$SONAR_JDBC_PWD'/' settings.xml
+
+#XVFB config
 export DISPLAY=:99
 #xvfb-run --server-args="-screen 0, 1366x768x24" selenium-standalone start
 
